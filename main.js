cd6a30d1cb98970d22183abb6ed0493e70338aa4
Michelle Totherow <tym4me13@gmail.com>
2023-03-08 15:15:27 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const { createMarble } = require("./marbleMaker.js")
+
+
+const marble = createMarble("small")
+console.log(marble)
+
+
+const { weaveBag } = require("./bagMaker.js")
+
+const bag = weaveBag("silk")
+console.log(bag)
